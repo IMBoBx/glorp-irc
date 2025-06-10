3e8e9c019e6fc6eb681f33760c2b006ea7d2f045
IMBoBx <garvkundnaniyt@gmail.com>
2025-06-10 05:43:39 +0530

    kinda made the server + a very barebones client testing

---
+++ client/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	crand "crypto/rand"
+	"fmt"
+	"log"
+	// "math/rand"
+	"net"
+	"time"
+)
+
+const MAX = 5
+const PORT = "8800"
+
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newConnection(i int, c chan bool) {
+	conn, err := net.Dial("tcp", "localhost:"+PORT)
+	handleErr(err)
+	defer conn.Close()
+
+	conn.Write(fmt.Appendf(nil, "NICK gorb%d\n", i))
+
+	counter := 0
+	for range time.Tick(3 * time.Second) {
+
+		conn.Write([]byte(crand.Text() + "\n"))
+
+		if counter++; counter == MAX {
+			conn.Write([]byte(fmt.Sprintf("EXIT gorb%d\n", i)))
+			c <- true
+			break
+		}
+	}
+}
+
+func main() {
+	done := make(chan bool, MAX)
+
+	for i := range MAX {
+		go newConnection(i, done)
+	}
+
+	for range MAX {
+		<-done
+	}
+}
